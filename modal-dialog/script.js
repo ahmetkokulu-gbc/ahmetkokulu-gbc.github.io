04cdce7418332ee9bd6b45ecee6a2d16b408e039
ahmetkokulu-gbc <123596204+ahmetkokulu-gbc@users.noreply.github.com>
2024-11-25 11:48:14 -0500

    Update script.js

--- modal-dialog/script.js
+++ modal-dialog/script.js
@@ -10,7 +10,7 @@ openBtn.addEventListener("click", () => {
 
 // Close the modal when clicking outside the modal
 modalContainer.addEventListener("click", (e) => {
-  console.log(e.target);
+  console.log("Modal Container Event Triggered ", e.target);
   if (e.target === modalContainer) {
     modalContainer.style.display = "none";
   }
@@ -18,6 +18,7 @@ modalContainer.addEventListener("click", (e) => {
 
 // Close the modal when clicking the close button
 closeBtn.addEventListener("click", (e) => {
+  console.log("Close Button Event Triggered ", e.target);
   // e.stopPropagation(); // Prevent event bubbling to modalContainer
   modalContainer.style.display = "none";
 });
